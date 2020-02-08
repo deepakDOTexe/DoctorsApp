7144bc4962caea7ce2efd92ff89654d6b4ed54c2
a-macc <iit2018121@iiita.ac.in>
2020-02-08 10:27:04 +0530

    Update build.gradle

---
+++ src/ai/kitt/snowboy/FormActivity.java
@@ -0,0 +1,14 @@
+package ai.kitt.snowboy;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import ai.kitt.snowboy.demo.R;
+
+public class FormActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.acitivity_form);
+    }
+}
